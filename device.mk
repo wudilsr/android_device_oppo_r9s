ce73ad1813ca770b0be6eb8574d19f44c459941c
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:14:14 +0200

    msm8953-common: Build Sensors HIDL multiHAL
     * Drop legacy HAL as no longer needed afterwards
    
    Change-Id: I42e8890f33f925b16c55169766acfef02034a0c5

--- device.mk
+++ device.mk
@@ -308,11 +308,12 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    sensors.msm8953
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf \
-    $(LOCAL_PATH)/sensors/hals.conf:system/etc/sensors/hals.conf
+    $(LOCAL_PATH)/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf \
+    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 # WiFi
 PRODUCT_PACKAGES += \
