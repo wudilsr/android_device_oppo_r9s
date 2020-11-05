ae87fa986a5720ccab31804abca523ec90f8f608
iamsaalim <saalimquadri1@gmail.com>
2020-11-05 15:54:28 +0530

    A57: Set AAPT config and screen info
    Change-Id: I14b381b6c3ff04028a0a3dccf75698bca6a806c4

--- device.mk
+++ device.mk
@@ -15,3 +15,11 @@ $(call inherit-product, vendor/oppo/A57/A57-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+
+# Screen density
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
