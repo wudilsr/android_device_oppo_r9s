8dbd62da2f2c2bb8180cfea5cb4109bb2183cacd
danascape <danascape@gmail.com>
2021-06-06 18:09:22 +0530

    A57: Import power_profile from A57_11_A.32_191205
    Change-Id: I813273ddb19f912dec91f1589dfd7509ea629b6c

--- device.mk
+++ device.mk
@@ -11,3 +11,7 @@ $(call inherit-product, device/oppo/msm8937-common/msm8937.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/oppo/A57/A57-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
